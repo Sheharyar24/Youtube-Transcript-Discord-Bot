7f8ee867cf249a764da4b2f3746e000a8c982c4b
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2025-07-26 00:21:12 +0400

    Feat : Add new video notification Used a loop to check for new videos from monitored channels every 5 minutes and modified the add channel function to send an embed message with the latest video details.

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
 from dotenv import load_dotenv
 from youtube_functions import *
@@ -13,13 +13,14 @@ token = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = 1397987625456898058
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-# List of monitoring channels
-Monitoring_Channels = []
+# Store a list of monitoring channels as dictionary with channel IDs and Latest video IDs
+Monitoring_Channels = {}
 
 @bot.event
 async def on_ready():
     print(f"Bot is online! Logged in as {bot.user}")
     channel = bot.get_channel(CHANNEL_ID)
+    my_loop.start()
     await channel.send("Bot is live")
 
 @bot.command(name='GetChannelStats')
@@ -38,11 +39,48 @@ async def get_channel_stats_command(ctx, channel_id: str):
 
 @bot.command(name='AddChannel')
 async def add_channel_command(ctx, channel_id: str):
-    """Add a channel to the database (not implemented)."""
-    Monitoring_Channels.append(channel_id)
+    """Add a channel to the database with its latest video Id."""
+    channel = bot.get_channel(CHANNEL_ID)
+    
+    latest_video = get_latest_uploaded_videos(channel_id, max_results=1)
+    Monitoring_Channels[channel_id]=latest_video[0]['videoId']
+
     stats = get_channel_stats(channel_id)
     stats = stats[0]
+
+    video_id = latest_video[0]['videoId']
+    published_at = latest_video[0]['publishedAt']
+    title = latest_video[0]['title']
+
+    embed = discord.Embed(title=title, description=f"Latest video uploaded at {published_at}", color=discord.Color.green())
+    embed.add_field(name="Video Link", value=f"https://www.youtube.com/watch?v={video_id}", inline=False)
+    await channel.send(embed=embed) 
+
     await ctx.send(f"Channel {stats['title']} added to the database.")
     print(Monitoring_Channels)
 
+@tasks.loop(minutes=5)
+async def my_loop():
+    """Check for new videos from monitored channels."""
+    channel = bot.get_channel(CHANNEL_ID)
+
+    for channel_id in Monitoring_Channels:
+        latest_video = get_latest_uploaded_videos(channel_id, max_results=1)
+        if latest_video:
+            video = latest_video[0]
+            if video['videoId'] != Monitoring_Channels[channel_id]:
+                Monitoring_Channels[channel_id] = video['videoId']
+                video_id = video['videoId']
+                published_at = video['publishedAt']
+                title = video['title']
+
+                embed = discord.Embed(title=title, description=f"New video uploaded at {published_at}", color=discord.Color.green())
+                embed.add_field(name="Video Link", value=f"https://www.youtube.com/watch?v={video_id}", inline=False)
+
+                await channel.send(embed=embed)
+            else:
+                pass
+            # Uncomment the following lines if you want to notify when no new videos are found  
+                # await channel.send(f"No new videos found for channel ID: {channel_id}")
+                # print(f"No new videos found for channel ID: {channel_id}")
 bot.run(token)
